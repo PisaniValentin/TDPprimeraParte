e024fcb7e4ee9bfc4e3202956428356981ec57cb
unknown <valentin.pisani@hotmail.com>
2020-09-18 19:33:12 -0300

    cambio para generara conflicto

--- TestPersona.java
+++ TestPersona.java
@@ -2,7 +2,7 @@ package PrimeraParte;
 
 public class TestPersona {
 public static void main(String []args) {
-	Persona humano = new Persona("Carlos",21);
+	Persona humano = new Persona("Valentin",24);
 	System.out.println("Nombre:"+humano.getNombre()+"Edad:"+humano.getEdad());
 }
 

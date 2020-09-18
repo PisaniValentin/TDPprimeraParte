4a765bc04d05e1ccf5c46fef5a843175d13bb097
unknown <valentin.pisani@hotmail.com>
2020-09-18 19:28:54 -0300

    borro archivo del repositorio

--- TestPersonaABorrar.java
+++
@@ -1,9 +0,0 @@
-package PrimeraParte;
-
-public class TestPersona {
-public static void main(String []args) {
-	Persona humano = new Persona("Alfredo",21);
-	System.out.println("Nombre:"+humano.getNombre()+"Edad:"+humano.getEdad());
-}
-
-}

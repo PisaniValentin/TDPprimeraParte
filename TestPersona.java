7992ad0341520dc036ef66d280ffddf1fcd3fe27
unknown <valentin.pisani@hotmail.com>
2020-09-18 19:38:19 -0300

    modifico el local para generar conflicto

--- TestPersona.java
+++ TestPersona.java
@@ -2,7 +2,7 @@ package PrimeraParte;
 
 public class TestPersona {
 public static void main(String []args) {
-	Persona humano = new Persona("Carlos",21);
+	Persona humano = new Persona("Francisco",22);
 	System.out.println("Nombre:"+humano.getNombre()+"Edad:"+humano.getEdad());
 }
 
